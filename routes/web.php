4ab5060a5f0382d6ecda3baf58d172f483e7dcc1
JoaoRebas <2150711@my.ipleiria.pt>
2018-05-17 19:03:55 +0100

    que é que se passa com esta coisa

--- routes/web.php
+++ routes/web.php
@@ -24,14 +24,12 @@ Route::patch('/profile/{user}', 'UserController@update')->name('update');
 Route::get('/history', 'PagesController@getHistory');
 Route::get('/camera', 'PagesController@getCamera');
 
+Route::get('history/mov1', 'AlarmController@alarms_movimento_1')->name('history.history_mov1');
+Route::get('history/mov2', 'AlarmController@alarms_movimento_2')->name('history.history_mov2');
+Route::get('history/flame', 'AlarmController@alarms_flame')->name('history.history_flame');
+Route::get('history/gas', 'AlarmController@alarms_gas')->name('history.history_gas');
 
 //Route::get('/profile/{user}', 'UserController@edit')
 
 Route::get('users/{user}',  ['as' => 'users.edit', 'uses' => 'UserController@edit']);
 Route::patch('users/{user}/update',  ['as' => 'users.update', 'uses' => 'UserController@update']);
-
-
-Route::get('history/mov1', 'AlarmController@alarms_movimento_1')->name('history.history_mov1');
-Route::get('history/mov2', 'AlarmController@alarms_movimento_2')->name('history.history_mov2');
-Route::get('history/flame', 'AlarmController@alarms_flame')->name('history.history_flame');
-Route::get('history/gas', 'AlarmController@alarms_gas')->name('history.history_gas');
