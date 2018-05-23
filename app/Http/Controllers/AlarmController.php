ee7fbbf02ec3568cebdd881de1dfd3bfbff8be98
JoaoRebas <2150711@my.ipleiria.pt>
2018-05-23 16:40:56 +0100

    Merge branch 'master' of https://github.com/JoaoRebas/projetoess

--- app/Http/Controllers/AlarmController.php
+++ app/Http/Controllers/AlarmController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers;
 
-
 use App\AlarmMovimento1;
 use App\AlarmMovimento2;
 use App\AlarmFlame;
 use App\AlarmGas;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class AlarmController extends Controller
 {
@@ -16,9 +16,43 @@ class AlarmController extends Controller
         $this->middleware('auth');
     }
 
+
+
     public function index()
     {
         //
+    }
+
+    public function createAlarmMov1(Request $request)
+    {
+        $alarmMov1 = new AlarmMovimento1();
+        $alarmMov1->fill($request->all());
+        $alarmMov1->created_at = Carbon::now();
+        $alarmMov1->save();
+    }
+
+    public function createAlarmMov2(Request $request)
+    {
+      $alarmMov1 = new AlarmMovimento2();
+      $alarmMov1->fill($request->all());
+      $alarmMov1->created_at = Carbon::now();
+      $alarmMov1->save();
+    }
+
+    public function createAlarmFlame(Request $request)
+    {
+      $alarmMov1 = new AlarmFlame();
+      $alarmMov1->fill($request->all());
+      $alarmMov1->created_at = Carbon::now();
+      $alarmMov1->save();
+    }
+
+    public function createAlarmGas(Request $request)
+    {
+      $alarmMov1 = new AlarmGas();
+      $alarmMov1->fill($request->all());
+      $alarmMov1->created_at = Carbon::now();
+      $alarmMov1->save();
     }
 
     public function alarms_movimento_1()
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/alarmMov1', 'AlarmController@createAlarmMov1');
+Route::post('/alarmMov2', 'AlarmController@createAlarmMov2');
+Route::post('/alarmFlame', 'AlarmController@createAlarmFlame');
+Route::post('/alarmGas', 'AlarmController@createAlarmGas');
